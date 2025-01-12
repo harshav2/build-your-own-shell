70b5d40975781a7b1e3266ee15f39918c8c82ad8
Harsha V <harsha.vishwanath2004@gmail.com>
2025-01-13 02:53:34 +0530

    Output correction

--- app/main.py
+++ app/main.py
@@ -17,8 +17,11 @@ def main():
             return 0
         if command_name == "echo":
             sys.stdout.write(" ".join(tokens[1:]))
-        if command_name == "type" and tokens[1] in builtin_commands:
-            sys.stdout.write(f"{tokens[1]} is a shell builtin")
+        if command_name == "type":
+            if tokens[1] in builtin_commands:
+                sys.stdout.write(f"{tokens[1]} is a shell builtin")
+            else:
+                sys.stdout.write(f"{tokens[1]}: not found")
         else:
             sys.stdout.write(f"{command_name}: command not found")
         sys.stdout.write('\n')

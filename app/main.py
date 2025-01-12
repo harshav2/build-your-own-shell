15b24b07af147f890ecd0c6f4242090b1651dbb1
Harsha V <harsha.vishwanath2004@gmail.com>
2025-01-13 02:31:23 +0530

    Stage 2 submission

--- app/main.py
+++ app/main.py
@@ -4,7 +4,14 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
-    input()
+    inp = input()
+
+    tokens = inp().split()
+
+    valid_commands = []
+
+    if tokens[0] not in valid_commands:
+        sys.stdout.write(f"{tokens[0]}: command not found")
 
 
 if __name__ == "__main__":

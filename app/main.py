d9b8e7bac2a01bf27eeaee78c9a92d8b41e795af
Harsha V <harsha.vishwanath2004@gmail.com>
2025-01-13 10:17:40 +0530

    Logical correction

--- app/main.py
+++ app/main.py
@@ -12,11 +12,8 @@ def find_executable(query_command):
     for path in paths:
         if os.path.isfile(f"{path}/{query_command}") and os.access(f"{path}/{query_command}", os.X_OK):
             return f"{path}/{query_command}"
+        
     return ""
-
-def handle_exit(tokens):
-    if tokens[1]==0:
-        return
 
 def handle_echo(tokens):
     sys.stdout.write(" ".join(tokens[1:]))
@@ -46,7 +43,7 @@ def main():
         command_name = tokens[0]
 
         if command_name == "exit":
-            handle_exit(tokens)
+            return tokens[1]
         
         elif command_name == "echo":
             handle_echo(tokens)
@@ -55,10 +52,11 @@ def main():
             handle_type(tokens)
                 
         else:
-            executable = find_executable(tokens[1])
+            executable = find_executable(tokens[0])
+            sys.stdout.write(f"Output: {executable}")
 
             if executable:
-                subprocess.run([command_name, *tokens[1:]])
+                subprocess.run([executable, *tokens[1:]])
             else:
                 sys.stdout.write(f"{command_name}: command not found")
 
